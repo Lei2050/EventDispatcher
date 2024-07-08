66f6e10c4ca6b66d9b78fe45f6fe1668919bb898
Lei <linhaibin676@gmail.com>
2024-07-08 14:24:30 +0800

    Update event_test.go

--- event_test.go
+++ event_test.go
@@ -12,20 +12,22 @@ func TestEventMgr(t *testing.T) {
 	)
 
 	em := NewEventMgr()
-	em.RegisterEvent(EVENT_ID_LEVEL_UP, func(a EventArg) {
+	f1 := func(a EventArg) {
 		var (
 			uid uint64 = a.GetUint64("uid")
 			lv  uint32 = a.GetUint32("lv")
 		)
 		t.Logf("levelup uid:%d, lv:%d", uid, lv)
-	})
-	em.RegisterEvent(EVENT_ID_CONSUME_MONEY, func(a EventArg) {
+	}
+	em.RegisterEvent(EVENT_ID_LEVEL_UP, f1)
+	f2 := func(a EventArg) {
 		var (
 			uid  uint64 = a.GetUint64("uid")
 			cost uint32 = a.GetUint32("cost")
 		)
 		t.Logf("consume money uid:%d, lv:%d", uid, cost)
-	})
+	}
+	em.RegisterEvent(EVENT_ID_CONSUME_MONEY, f2)
 	em.RegisterEvent(EVENT_ID_CONSUME_ITEM, func(a EventArg) {
 		var (
 			uid    uint64 = a.GetUint64("uid")
@@ -39,6 +41,13 @@ func TestEventMgr(t *testing.T) {
 	em.SendEvent(EVENT_ID_CONSUME_MONEY, map[string]interface{}{"uid": uint64(12138), "cost": uint32(1000000)})
 	//em.SendEvent2(EVENT_ID_CONSUME_ITEM, &ConsumeItemParam{Uid: 12138, ItemId: 110, Num: 5})
 	em.SendEvent2(EVENT_ID_CONSUME_ITEM, NewEventArg().Add("uid", 12138).Add("item_id", 110).Add("num", 5))
+
+	em.UnregisterEvent(EVENT_ID_LEVEL_UP, f1).UnregisterEvent(EVENT_ID_CONSUME_MONEY, f2).UnregisterEvent(EVENT_ID_LEVEL_UP, f1).UnregisterEvent(EVENT_ID_CONSUME_ITEM, f2)
+	em.SendEvent(EVENT_ID_LEVEL_UP, map[string]interface{}{"uid": 12138, "lv": 99})
+	em.SendEvent(EVENT_ID_CONSUME_MONEY, map[string]interface{}{"uid": uint64(12138), "cost": uint32(1000000)})
+	em.SendEvent2(EVENT_ID_CONSUME_ITEM, NewEventArg().Add("uid", 12138).Add("item_id", 110).Add("num", 5))
+
+	t.Logf("============================================")
 
 	RegisterEventS(EVENT_ID_LEVEL_UP, func(a EventArg) {
 		var (
